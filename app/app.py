ff0c488f1acb39d76264ab4d14dee7ba8d17c22a
Vishan04 <sreevishan29@gmail.com>
2026-09-19 13:12:59 +0530

    update apllication to version 2

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Kubernetes Jenkins Rollback Project - Version 1"
+    return "Kubernetes Jenkins Rollback Project - Version 2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
